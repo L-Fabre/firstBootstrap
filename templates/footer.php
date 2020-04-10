d659351f843fd957f6bf814efe54705b0979fb2e
L-Fabre <l.fabre@it-studants.fr>
2020-04-10 11:14:23 +0200

    1

---
+++ templates/footer.php
@@ -0,0 +1,55 @@
+<!-- Footer -->
+<footer class="page-footer font-small blue pt-4">
+
+    <!-- Footer Links -->
+    <div class="container-fluid text-center text-md-left">
+
+    <!-- Grid row -->
+    <div class="row">
+
+    <!-- Grid column -->
+    <div class="col-md-6 mt-md-0 mt-3">
+
+        <!-- Content -->
+        <h5 class="text-uppercase">Lorem</h5>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus eveniet ea ipsa, culpa odit inventore soluta delectus veniam sint debitis natus autem excepturi repellat necessitatibus adipisci ad quaerat, nobis doloremque!</p>
+
+    </div>
+    <!-- Grid column -->
+
+    <hr class="clearfix w-100 d-md-none pb-3">
+
+    <!-- Grid column -->
+    <div class="col-md-3 mb-md-0 mb-3">
+
+        <ul class="list-unstyled">
+            <li>
+            
+            </li>
+        </ul>
+
+    </div>
+        <div class="col-md-3 mb-md-0 mb-3">
+        <h5 class="text-uppercase">GreenLife</h5>
+        <ul class="list-unstyled">
+            <li>
+                <a href="interieur.php">Interieur</a>
+                <br>
+                <a href="exterieur.php">Exterieur</a>
+            </li>
+        </ul>
+
+        </div>
+    </div>
+    <!-- Grid row -->
+
+  </div>
+
+
+  <!-- Copyright -->
+  <div class="footer-copyright text-center py-3">© 2020 Copyright
+  </div>
+  <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
